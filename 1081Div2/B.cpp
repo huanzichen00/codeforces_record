39d889d5b2b4981e9029e6776ef102be3a64a4b9
HuanZZZ <huanzichen00@gmail.com>
2026-02-25 10:50:52 +0800

    filp_binary_another_try

--- 1081Div2/B.cpp
+++ 1081Div2/B.cpp
@@ -1,12 +1,48 @@
-include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int tt;
-  cin >> tt;
-  
+  int n, string;
+  cin >> n, string;
+
+  int zeros = 0, ones = 0;
+  int idx1[n];
+  int limit = 0;
+  for (int i = 0; i < n; i++) {
+    if (s[i] == 1) {
+      ones++;
+      idx1[i] = limit++;
+    }
+  }
+  zeros = n - ones;
+  if (n % 2 == 1 && ones % 2 == 0) {
+    cout << "-1" << "\n";
+    return 0;
+  }
+  if (ones % 2 == 0) {
+    cout << ones << "\n";
+    int limit1 = 0;
+    for (int i = 0; i < n; i++) {
+      if (i == idx1[limit1]) {
+        cout << i << " ";
+        limit1++;
+      }
+    }
+    cout << "\n";
+    return 0;
+  }
+  cout << zeros << "\n";
+  int limit2 = 0;
+  for (int i = 0; i < n; i++) {
+    if (i != idx1[limit2]) {
+      cout << i << " ";
+    } else {
+      limit2++;
+    }
+  }
+  cout << "\n";
   return 0;
 }
