9950aa79ec83b00c6fd6e00868615d468511f8d6
HuanZZZ <huanzichen00@gmail.com>
2026-02-25 10:58:13 +0800

    bugs

--- 1081Div2/B.cpp
+++ 1081Div2/B.cpp
@@ -1,32 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int n; string s;
+  int n;
+  string s;
   cin >> n >> s;
 
   int zeros = 0, ones = 0;
   int idx1[n];
   int limit = 0;
-  for (int i = 0; i < n; i++) {
-    if (s[i] == 1) {
+  for (int i = 0; i < n; i++)
+  {
+    if (s[i] == '1')
+    {
       ones++;
-      idx1[i] = limit++;
+      idx1[limit++] = i;
     }
   }
   zeros = n - ones;
-  if (n % 2 == 1 && ones % 2 == 1) {
+  if (n % 2 == 1 && ones % 2 == 1)
+  {
     cout << "-1" << "\n";
     return 0;
   }
-  if (ones % 2 == 0) {
+  if (ones % 2 == 0)
+  {
     cout << ones << "\n";
     int limit1 = 0;
-    for (int i = 0; i < n; i++) {
-      if (i == idx1[limit1]) {
+    for (int i = 0; i < n; i++)
+    {
+      if (i == idx1[limit1])
+      {
         cout << i + 1 << " ";
         limit1++;
       }
@@ -36,10 +44,14 @@ int main() {
   }
   cout << zeros << "\n";
   int limit2 = 0;
-  for (int i = 0; i < n; i++) {
-    if (i != idx1[limit2]) {
+  for (int i = 0; i < n; i++)
+  {
+    if (i != idx1[limit2])
+    {
       cout << i + 1 << " ";
-    } else {
+    }
+    else
+    {
       limit2++;
     }
   }
